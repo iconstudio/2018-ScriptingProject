204b513b84e6116c804816478b15cd3fef2ee1c1
iconstudio <yoyofa2@hotmail.com>
2018-05-30 04:03:54 +0900

    Squashed commit of the following: 발표 자료 commit e8f489bca96f236a9acebdaa40679a590946c4c4 Merge: 89ea7d9 a825f5d Author: Iconer <yoyofa2@hotmail.com> Date:   Wed May 30 03:58:31 2018 +0900
        Merge pull request #4 from junstar96/master
    
        읽을 수 있게 해놨어요.
    
    commit a825f5d5ed0cb1794b624fcfb9434cb954fda94e
    Author: junstar <junstar96@naver.com>
    Date:   Tue May 29 22:09:39 2018 +0900
    
        that read
    
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

--- src/main_loader.py
+++ src/main_loader.py
@@ -2,16 +2,12 @@ from import_file import *
 import petron_loader as pet
 import army_loader as army
 
-
-
-
 def start_element(name, attrs):
 	print('Start element:', name, attrs)
 
 
 def char_data(data):
 	print('Character data:', repr(data))
-
 
 # pa = xml.parsers.expat.ParserCreate()
 # pa.StartElementHandler = start_element  # 이벤트 핸들러 연결
