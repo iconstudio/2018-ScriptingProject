6c847ab0e1a2a30a31b20d55d189578f8040dfa7
DESKTOP-AM8BPM8\yoyof <yoyofa2@hotmail.com>
2018-05-30 09:19:37 +0900

    modified.

--- src/main.py
+++ src/main.py
@@ -34,54 +34,6 @@ from main_loader import *
 try:
 	if __name__ == "__main__":
 		print("begins")
-
-		for x in range(1, 6):
-			print(x, '*', x, '=', str(x * x).zfill(3))
-
-		dic = {"item": "apple", "color": "red"}
-		print("{0[item]} is {0[color]}".format(dic))
-
-		item = "apple"
-		_할로 = "red"
-		print("{0[item]} is {0[_할로]}".format(locals()))
-		print("{0:c}".format(66))
-		print("{0:.4f}".format(1 / 3 * 3))
-
-		# with open('test.txt') as f:  # with 코드블록 as는 파일 핸들 지정
-		#    print(f.readlines())
-		# print(f.closed)
-
-		colors = ['red', 'green', 'black']
-		f = open('file_colors', 'wb')  # pickle 을 위해 바이너리 쓰기 파일 오픈
-		pickle.dump(colors, f)  # colors 리스트를 file_colors로 dump
-		f.close()
-		del colors
-
-
-		class SuperTest:
-			pass
-
-		class Test(SuperTest):
-			var1 = 0
-			var2 = "43571aa826aa439912"
-			var3 = 2397034024519028
-
-		a = Test()
-
-		f = open('file_Test', 'wb')
-		pickle.dump(a, f)
-		f.close()
-		del a
-
-		f = open('file_Test', 'rb')  # pickle을 위해 바이너리 읽기 파일 오픈
-		a = pickle.load(f)  # 파일에서 리스트 load
-		f.close()
-		print(a)
-		print(f.closed)
-		print(a.var1)
-		print(a.var2)
-		print(a.var3)
-
 	else:
 		raise RuntimeError
 except RuntimeError:
