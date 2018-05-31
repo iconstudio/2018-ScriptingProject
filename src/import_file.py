ce5d2af2b964c0ed1f2a2a3c339fe47358655276
DESKTOP-AM8BPM8\yoyof <yoyofa2@hotmail.com>
2018-05-31 11:03:17 +0900

    modified.

--- src/import_file.py
+++ src/import_file.py
@@ -1,21 +1,57 @@
 import math
+import sys
 import os
 import io
-import sys
+
 sys.path.append('included_header')
+
 from socket import *
 import urllib.request
 import urllib.parse
-from PIL import Image, ImageTk
 import folium
+import json
+import pickle
 from xml.dom.minidom import parse, parseString
 from xml.etree import cElementTree
 import xml
 import xmlrpc
 import xml.parsers.expat
-import json
-import pickle
 
 import tkinter
 from tkinter import font
 from tkinter import messagebox
+
+
+class xml_open:
+	def __init__(self):
+		print("상속됨")
+
+	def open_xml(self):
+		pass
+
+
+def make_button(hwnd, caption: str, nx: int, ny: int, swidth: str, sheight: str, cmd=None) -> tkinter.Widget:
+	global global_font
+	newbutton = tkinter.Button(hwnd, background="#CCCCCC", activebackground="#7A7A7A", highlightcolor="#7A7A7A",
+	                           highlightthickness="2", disabledforeground="#FFFFFF", font=global_font,
+	                           borderwidth="0", overrelief="flat", relief="flat", text=caption)
+	newbutton.pack()
+	newbutton.config(width=swidth, height=sheight, command=cmd)
+	newbutton.place(x=nx, y=ny)
+	return newbutton
+
+
+def component_pack(what: tkinter.Widget):
+	what.pack()
+
+
+def component_hide(what: tkinter.Widget):
+	what.pack_forget()
+
+
+def component_command_set(what: tkinter.Widget, cmd):
+	what.config(command=cmd)
+
+
+def component_name_change(what: tkinter.Widget, how: str):
+	what.config(text=how)
--- src/petron_loader.py
+++ src/petron_loader.py
@@ -1,117 +1,151 @@
 from import_file import *
 
-
-
-xml_cont_pass = "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D" #복무기관 입력키
-xml_cont_endpoint = "http://apis.data.go.kr/1300000/bmggJeongBo"
-cont_url = "http://apis.data.go.kr/1300000/bmggJeongBo/list"
-pet_loopflag_cont = 1
-contdocs = None
-cont_dom = None
-cont_tet = urllib.parse.quote("경남")
-
-xml_null_pass = "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D" #복무공석 입력키
-xml_null_endpoint = "http://apis.data.go.kr/1300000/bistGongseok/list"
-null_url = "http://apis.data.go.kr/1300000/bistGongseok/list/bistGongseok/list"
-pet_loopflag_null = 1
-nulldocs = None
-null_dom = None
-null_tet = urllib.parse.quote("경남")
-
-def open_null_xml():
-	global nulldocs
-	global null_dom
-	global null_tet
-	# +"&numOfRows=10&pageSize=10&pageNo=1&startPage=1" 이건 나중에 써보고 다시 편집
-	try:
-		pet_null = urllib.request.Request(null_url + "?serviceKey=" + xml_null_pass + "&numOfRows=10&pageSize=10&pageNo=1&startPage=1")
-
-		with urllib.request.urlopen(pet_null) as t:
-			raw_data = t.read()
-			t.close()
-			null_dom = parseString(raw_data)
-			print(null_dom.toprettyxml())
-
-	except IOError:
-	  print("null_error")
-	  return None
-	except urllib.error.URLError as e:
-		print(e.reason)
-		print(parseString(e.read().decode('utf-8')).toprettyxml())
-	except urllib.error.HTTPError as e:
-		print("error code = " + e.reason)
-		print(parseString(e.read().decode('utf-8')).toprettyxml())
-
-def print_null_xml():
-	global null_dom
-	global nulldocs
-	nulldocs = null_dom.childNodes
-
-
-def open_cont_xml():
-	global contdocs
-	global cont_dom
-
-	try:
-		pet_cont = urllib.request.Request(cont_url + "?serviceKey=" + xml_cont_pass)
-		with urllib.request.urlopen(pet_cont) as t:
-			raw_data = t.read()
-			t.close()
-			cont_dom = parseString(raw_data)
-			print(cont_dom.toprettyxml())
-	except IOError:
-	  print("cont_error")
-	  return None
-	except urllib.error.URLError as e:
-		print(e.reason)
-		print(parseString(e.read().decode('utf-8')).toprettyxml())
-	except urllib.error.HTTPError as e:
-		print("error code = " + e.reason)
-		print(parseString(e.read().decode('utf-8')).toprettyxml())
-
-
+window = None
+buttons = dict()
 
 wx = int((1920 - 960) * 0.5)
 wy = int((1080 - 540) * 0.5)
-petronwindow = tkinter.Tk()
-petronwindow.title("Petron_window")
-petronwindow.geometry("960x540+" + str(wx) + "+" + str(wy))
-petronwindow.resizable(0, 0)
-petronwindow.minsize(960, 540)
-petronwindow.maxsize(960, 540)
-Font = font.Font(petronwindow, size=12, weight='normal', family='NanumGothic')
 
 
-open_null_xml()
-print_null_xml()
-open_cont_xml()
+class pet_cont:
+	def __init__(self, master):
+		self.master = master
+		self.xml_pass = "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D"  # 복무기관 입력키
+		self.xml_url = "http://apis.data.go.kr/1300000/bmggJeongBo/list"
+		self.loopflag = 1
+		self.docs = None
+		self.doms = None
+		self.search_key = urllib.parse.quote("경남")
 
-Caption = tkinter.Label(petronwindow, font=Font, text="목록")
-Caption.place(x=wx, y=20)
+	def open_xml(self):
+		# +"&numOfRows=10&pageSize=10&pageNo=1&startPage=1" 이건 나중에 써보고 다시 편집
+		try:
+			pet_null = urllib.request.Request(self.xml_url + "?serviceKey=" + self.xml_pass)
+		except IOError:
+			print("null_error")
+			return None
+		except urllib.error.URLError as e:
+			print(e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		except urllib.error.HTTPError as e:
+			print("error code = " + e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		else:
+			with urllib.request.urlopen(pet_null) as t:
+				raw_data = t.read()
+				t.close()
+				army_dom = parseString(raw_data)
+				row = army_dom.childNodes
+				pry = row[0].childNodes
+				head = pry[0].childNodes
+				cla = pry[1].childNodes
+				print("header")
+				for i in head:
+					print(i)
+				print("body")
+				for i in cla:
+					print(i)
 
-button_military = tkinter.Button(petronwindow, text="공석 확인", command=None)
-button_military.pack()
-button_military.config(width="18", height="8")
-button_military.place(x=480 - 200, y=80)
+	def open_window(self):
+		self.frame = tkinter.Toplevel(self.master)
+		self.quitButton = tkinter.Button(self.frame, text='Quit', width=25, command=self.close_windows)
+		self.quitButton.pack()
 
-button_path = tkinter.Button(petronwindow, text="사회복무 회사 검색", command=None)
-button_path.pack()
-button_path.config(width="18", height="8")
-button_path.place(x=480 + 8, y=80)
-
-button_milinfo = tkinter.Button(petronwindow, text="현역 정보", command=None)
-button_milinfo.pack()
-button_milinfo.config(width="18", height="8")
-button_milinfo.place(x=480 - 200, y=300)
-
-button_pubinfo = tkinter.Button(petronwindow, text="사회복무 정보", command=None)
-button_pubinfo.pack()
-button_pubinfo.config(width="18", height="8")
-button_pubinfo.place(x=480 + 8, y=300)
+	def close_windows(self):
+		self.master.destroy()
 
 
-petronwindow.mainloop()
+class pet_null:
+	def __init__(self, master):
+		self.master = master
+		self.xml_pass = "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D"  # 복무공석 입력키
+		self.xml_url = "http://apis.data.go.kr/1300000/bistGongseok/list/bistGongseok/list"
+		self.loopflag = 1
+		self.docs = None
+		self.doms = None
+		self.search_key = urllib.parse.quote("경남")
+
+	def open_xml(self):
+		# +"&numOfRows=10&pageSize=10&pageNo=1&startPage=1" 이건 나중에 써보고 다시 편집
+		try:
+			pet_null = urllib.request.Request(self.xml_url + "?serviceKey=" + self.xml_pass)
+		except IOError:
+			print("null_error")
+			return None
+		except urllib.error.URLError as e:
+			print(e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		except urllib.error.HTTPError as e:
+			print("error code = " + e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		else:
+			with urllib.request.urlopen(pet_null) as t:
+				raw_data = t.read()
+				t.close()
+				army_dom = parseString(raw_data)
+				row = army_dom.childNodes
+				pry = row[0].childNodes
+				head = pry[0].childNodes
+				cla = pry[1].childNodes
+				print("header")
+				for i in head:
+					print(i)
+				print("body")
+				for i in cla:
+					print(i)
+
+	def open_window(self):
+		self.frame = tkinter.Frame(self.master)
+		self.quitButton = tkinter.Button(self.frame, text='Quit', width=25, command=self.close_windows)
+		self.quitButton.pack()
+		self.frame.pack()
+
+	def close_windows(self):
+		self.master.destroy()
 
 
+def buttons_show_all():
+	global buttons
+	component_pack(buttons["military"])
+	component_pack(buttons["milinfo"])
+	component_pack(buttons["path"])
+	component_pack(buttons["pubinfo"])
+	component_pack(buttons["calculator"])
 
 
+def buttons_hide_all():
+	global buttons
+	component_hide(buttons["military"])
+	component_hide(buttons["milinfo"])
+	component_hide(buttons["path"])
+	component_hide(buttons["pubinfo"])
+	component_hide(buttons["calculator"])
+
+
+def main():
+	global window, wx, wy, global_font
+	window = tkinter.Tk()
+	window.title("Petron_window")
+	window.geometry("960x540+" + str(wx) + "+" + str(wy))
+	window.resizable(0, 0)
+	window.minsize(960, 540)
+	window.maxsize(960, 540)
+	global_font = font.Font(window, size=12, weight='normal', family='NanumGothic')
+
+	roll = pet_cont(window)
+	roll.open_xml()
+
+	Caption = tkinter.Label(window, font=global_font, text="목록")
+	Caption.place(x=wx, y=20)
+
+	buttons["military"] = make_button(window, "현역 판정 검사", 280, 80, "18", "7", buttons_hide_all)
+	buttons["milinfo"] = make_button(window, "현역 정보", 280, 300, "18", "7")
+	buttons["path"] = make_button(window, "훈련소 가는 길", 488, 80, "18", "7")
+	buttons["pubinfo"] = make_button(window, "사회 복무 정보", 488, 300, "18", "7", roll.open_window)
+	buttons["calculator"] = make_button(window, "근무 일자\n계산", 8, 442, "10", "3")
+
+	window.mainloop()
+
+
+if __name__ == '__main__':
+	main()
