da8c175a740bd75d5acc06a72f0f993d682ffd08
junstar <junstar96@naver.com>
2018-06-11 21:21:17 +0900

    2018-06-11

--- src/import_file.py
+++ src/import_file.py
@@ -6,12 +6,9 @@ import io
 sys.path.append('included_header')
 sys.path.append(".idea")
 
-from socket import *
+import webbrowser
 import urllib.request
 import urllib.parse
-import folium
-import json
-import pickle
 from xml.dom.minidom import parse, parseString
 from xml.etree import ElementTree
 import xml
@@ -22,102 +19,100 @@ import http.client, http.server
 import tkinter
 from tkinter import *
 from tkinter import font
+import time
 
-def make_button(hwnd, caption: str, nx: int, ny: int, swidth: str, sheight: str, cmd = None) -> Widget:
-    newbutton = Button(hwnd, background = "#CCCCCC", activebackground = "#7A7A7A", highlightcolor = "#7A7A7A",
-                       highlightthickness = "2", disabledforeground = "#FFFFFF", borderwidth = "0", overrelief = "flat",
-                       relief = "flat", text = caption)
-    newbutton.pack()
-    newbutton.config(width = swidth, height = sheight, justify = "center", command = cmd)
-    newbutton.place(x = nx, y = ny)
-    return newbutton
-
-
-def make_button_grid(hwnd, caption: str, r: int, c: int, swidth: str, sheight: str, cmd = None) -> Widget:
-    newbutton = Button(hwnd, background = "#CCCCCC", activebackground = "#7A7A7A", highlightcolor = "#7A7A7A",
-                       highlightthickness = "2", disabledforeground = "#FFFFFF", borderwidth = "0", overrelief = "flat",
-                       relief = "flat", text = caption)
-    newbutton.config(width = swidth, height = sheight, justify = "center", command = cmd)
-    newbutton.grid(row = r, column = c, pady = 4, padx = 4)
-    return newbutton
+def make_button(hwnd, caption: str, nx: int, ny: int, swidth: str, sheight: str, cmd=None) -> Button:
+	newbutton = Button(hwnd, background="#CCCCCC", activebackground="#7A7A7A", highlightcolor="#7A7A7A",
+	                   highlightthickness="2", disabledforeground="#FFFFFF", borderwidth="0", overrelief="flat",
+	                   relief="flat", text=caption)
+	newbutton.pack()
+	newbutton.config(width=swidth, height=sheight, justify="center", command=cmd)
+	newbutton.place(x=nx, y=ny)
+	return newbutton
 
 
-def component_pack(what: Widget):
-    what.pack()
+def make_button_grid(hwnd, caption: str, r: int, c: int, swidth: str, sheight: str, cmd=None) -> Button:
+	newbutton = Button(hwnd, background="#CCCCCC", activebackground="#7A7A7A", highlightcolor="#7A7A7A",
+	                   highlightthickness="2", disabledforeground="#FFFFFF", borderwidth="0", overrelief="flat",
+	                   relief="flat", text=caption)
+	newbutton.config(width=swidth, height=sheight, justify="center", command=cmd)
+	newbutton.grid(row=r, column=c, pady=4, padx=4)
+	return newbutton
 
 
-def component_hide(what: Widget):
-    what.pack_forget()
+def make_listbox(hwnd, r: int, c: int) -> Listbox:
+	scrollbar = Scrollbar(hwnd)
+	scrollbar.grid(row=r, column=c + 1, padx=0, sticky=N + S)
+
+	listbox = Listbox(hwnd, borderwidth="0", relief="flat", activestyle='none', yscrollcommand=scrollbar.set)
+	listbox.grid(row=r, column=c, padx=0)
+	scrollbar.config(command=listbox.yview)
+
+	return listbox
 
 
-def component_command_set(what: Widget, cmd):
-    what.config(command = cmd)
-
-
-def component_name_change(what: Widget, how: str):
-    what.config(text = how)
-
-
-def openAPIparse(string, parser = None):
-    if parser is None:
-        from xml.dom import expatbuilder
-        return expatbuilder.parseString(string)
-    else:
-        from xml.dom import pulldom
-        return pulldom.parseString()
+def openAPIparse(string, parser=None):
+	if parser is None:
+		from xml.dom import expatbuilder
+		return expatbuilder.parseString(string)
+	else:
+		from xml.dom import pulldom
+		return pulldom.parseString()
 
 
 class xml_parser:
-    frame = None
-    button_quit = None
-    xml_url = ""
-    xml_pass = ""
-    search_key = ""
+	frame = None
+	button_quit = None
+	xml_url = ""
+	xml_pass = ""
+	search_key = ""
 
-    def __init__(self, master: Tk):
-        self.master = master
-        self.loopflag = 1
-        self.docs = None
-        self.doms = None
-        self.childbody = None
-        self.childhead = None
-        self.nextpage = 1
-        self.datasize = 1000
-        self.pagesize = 100
+	def __init__(self, master: Tk):
+		self.master = master
+		self.loopflag = 1
+		self.docs = None
+		self.doms = None
+		self.childbody = None
+		self.childhead = None
+		self.nextpage = 1
+		self.datasize = 10
+		self.pagesize = 10
 
-    def xml_connect(self, url: str, passcode: str, key = ""):
-        self.xml_url = url
-        self.xml_pass = passcode
-        self.search_key = key
+	def xml_connect(self, url: str, passcode: str, key=""):
+		self.xml_url = url
+		self.xml_pass = passcode
+		self.search_key = key
 
-        # +"&numOfRows=10&pageSize=10&pageNo=1&startPage=1" 이건 나중에 써보고 다시 편집
-        try:
-            pet_null = urllib.request.Request(self.xml_url + "?serviceKey=" + self.xml_pass
-                                              + "&numOfRows={0}&pageSize={1}&pageNo={2}&startPage=1".format(
-                self.datasize, self.datasize,
-                self.nextpage))  # 이건 나중에 키 입력을 통해 변경할 수 있도록
-        except IOError:
-            print("null_error")
-            return None
-        except urllib.error.URLError as e:
-            print(e.reason)
-            print(parseString(e.read().decode('utf-8')).toprettyxml())
-        except urllib.error.HTTPError as e:
-            print("error code = " + e.reason)
-            print(parseString(e.read().decode('utf-8')).toprettyxml())
-        else:
-            with urllib.request.urlopen(pet_null) as t:
-                test_tree = ElementTree.ElementTree(file = t)
-                self.childbody = test_tree.getroot()
+		# +"&numOfRows=10&pageSize=10&pageNo=1&startPage=1" 이건 나중에 써보고 다시 편집
+		try:
+			pet_null = urllib.request.Request(self.xml_url + "?serviceKey=" + self.xml_pass
+			                                  + "&numOfRows={0}&pageSize={1}&pageNo={2}&startPage=1".format(
+				self.datasize, self.datasize,
+				self.nextpage))  # 이건 나중에 키 입력을 통해 변경할 수 있도록
+		except IOError:
+			print("null_error")
+			return None
+		except urllib.error.URLError as e:
+			print(e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		except urllib.error.HTTPError as e:
+			print("error code = " + e.reason)
+			print(parseString(e.read().decode('utf-8')).toprettyxml())
+		else:
+			with urllib.request.urlopen(pet_null) as t:
+				test_tree = ElementTree.ElementTree(file=t)
+				self.childbody = test_tree.getroot()
 
-    def window(self, newtitle: str):
-        self.frame = Toplevel(self.master)
-        self.frame.title(newtitle)
-        self.frame.resizable(0, 0)
+	def window(self, newtitle: str) -> Widget:
+		self.frame = Toplevel(self.master)
+		self.frame.title(newtitle)
+		self.frame.geometry("480x540+%d+%d" % (80, 80))
+		self.frame.resizable(0, 0)
+		self.frame.minsize(480, 540)
 
-        self.button_quit = make_button_grid(self.frame, "종료", 5, 5, "8", "2", self.__del__).grid(sticky = W + E,
-                                                                                                 pady = 10)
-        return self.frame
+		self.button_quit = make_button_grid(self.frame, "종료", 5, 5, "8", "2", self.__del__).grid(sticky=W + E,
+		                                                                                         pady=10)
+		return self.frame
 
-    def __del__(self):
-        self.frame.destroy()
+	def __del__(self):
+		self.frame.destroy()
--- src/petron_loader.py
+++ src/petron_loader.py
@@ -3,26 +3,34 @@ from import_file import *
 window = None
 buttons = dict()
 
+
 def main():
     global window, wx, wy, global_font
     window = Tk()
     window.title("Call of Duty")
-    wx = 0 # int((1920 - 960) // 4)
-    wy = 80  # int((window.winfo_height() - 540) * 0.5)
+    wx = 80
+    wy = 80
 
     window.geometry("960x540+%d+%d" % (wx, wy))
     window.bind("<Escape>", lambda self: self.widget.quit())
     window.resizable(0, 0)
     window.minsize(960, 540)
     window.maxsize(960, 540)
-    window.configure(background = '#ffffff')
+    window.configure(background='#ffffff')
     tkpi = BitmapImage("background.jpg")
-    bg = Label(window, image = tkpi)
-    bg.place(x = 0, y = 0)
+    bg = Label(window, image=tkpi)
+    bg.place(x=0, y=0)
     bg.pack()
 
+    training_spot = [
+        "전진", "노도", "백골", "열쇠", "청성", "칠성", "오뚜기", "백마", "화랑", "을지", "승리", "번개", "결전",
+        "백두산", "율곡", "철벽", "비룡", "불무리", "이기자", "태풍", "필승", "충장", "백룡", "충경", "백호", "충용",
+        "충무", "강철", "전승", "충렬", "봉화", "맹호",
+        "육군 입영 훈련소", "해병대 교육 훈련단", "해군 교육 사령부", "공군 교육 사령부"
+    ]
+
     window.focus_set()
-    global_font = font.Font(window, size = 14, weight = 'normal', family = 'NanumGothic')
+    global_font = font.Font(window, size=14, weight='normal', family='NanumGothic')
 
     print(window)
 
@@ -35,33 +43,59 @@ def main():
     def make_popup_military():
         get = make_popup("현역 정보")
         get[0].xml_connect("http://apis.data.go.kr/1300000/gbSeoryu/list/gbSeoryu/list",
-                           "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D", urllib.parse.quote("경남"))
+                           "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D",
+                           urllib.parse.quote("경남"))
 
-        InputLabel = Entry(get[1], font = global_font, width = 25, borderwidth = 12, relief = 'flat')
-        InputLabel.grid(row = 0, column = 5)
+        InputLabel = Entry(get[1], font=global_font, width=25, borderwidth=12, relief='flat')
+        InputLabel.grid(row=0, column=5)
 
-        datalist = dict(gsteukgiCd = [], gsteukgiNm = [], gunGbnm = [], jcseoryuNm = [], mjbgteukgiNm = [])
-        for i in get[0].childbody.iter("item"):
-            datalist["gsteukgiCd"].append("{0}".format(i.findtext("gsteukgiCd")))
-            datalist["gsteukgiNm"].append("{0}".format(i.findtext("gsteukgiNm")))
-            datalist["gunGbnm"].append("{0}".format(i.findtext("gunGbnm")))
-            datalist["jcseoryuNm"].append("{0}".format(i.findtext("jcseoryuNm")))
+        datalist = dict(gsteukgiCd=[], gsteukgiNm=[], gunGbnm=[], jcseoryuNm=[], mjbgteukgiNm=[])
 
-        for i in range(0, len(datalist["gsteukgiCd"])):
-            print("군사 특기 코드 : {0}".format(datalist["gsteukgiCd"][i]))
-            print("군사 특기명 : {0}".format(datalist["gsteukgiNm"][i]))  # 기준1
-            print("해당 부서 : {0}".format(datalist["gunGbnm"][i]))  # 기준2
-            print("제출 서류 : {0}".format(datalist["jcseoryuNm"][i]))
 
-        make_button_grid(get[1], "<", 1, 4, "2", "2")
-        make_button_grid(get[1], "조회", 1, 5, "4", "2")
-        make_button_grid(get[1], ">", 1, 6, "2", "2")
+        def movenext():
+            get[0].nextpage += 1
+            page_num.configure(text=get[0].nextpage)
+
+        def printlist():
+            print(get[0].nextpage)
+            get[0].xml_connect("http://apis.data.go.kr/1300000/gbSeoryu/list/gbSeoryu/list",
+                               "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D",
+                               urllib.parse.quote("경남"))
+            for i in get[0].childbody.iter("item"):
+                datalist["gsteukgiCd"].append("{0}".format(i.findtext("gsteukgiCd")))
+                datalist["gsteukgiNm"].append("{0}".format(i.findtext("gsteukgiNm")))
+                datalist["gunGbnm"].append("{0}".format(i.findtext("gunGbnm")))
+                datalist["jcseoryuNm"].append("{0}".format(i.findtext("jcseoryuNm")))
+
+            print(datalist["gsteukgiCd"][0])
+            for i in range(0, len(datalist["gsteukgiCd"])):
+                print("군사 특기 코드 : {0}".format(datalist["gsteukgiCd"][i]))
+                print("군사 특기명 : {0}".format(datalist["gsteukgiNm"][i]))  # 기준1
+                print("해당 부서 : {0}".format(datalist["gunGbnm"][i]))  # 기준2
+                print("제출 서류 : {0}".format(datalist["jcseoryuNm"][i]))
+
+        def moveback():
+            global datalist
+            if get[0].nextpage > 0:
+                get[0].nextpage -= 1
+                page_num.configure(text=get[0].nextpage)
+
+        get[1].update_idletasks()
+        time.sleep(2)
+
+        page_num = Label(get[1], text = get[0].nextpage)
+        page_num.grid(row = 4, column = 5)
+        make_button_grid(get[1], "<", 1, 4, "2", "2", moveback)
+        make_button_grid(get[1], "조회", 1, 5, "4", "2", printlist)
+        make_button_grid(get[1], ">", 1, 6, "2", "2", movenext)
         return get[1]
 
     def make_popup_milinfo():
-        get = make_popup("현역 정보")
+        get = make_popup("현역 서류")
         get[0].xml_connect("http://apis.data.go.kr/1300000/gbSeoryu/list/gbSeoryu/list"
-                           , "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D", urllib.parse.quote("경남"))
+                           ,
+                           "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D",
+                           urllib.parse.quote("경남"))
 
         make_button_grid(get[1], "<", 1, 4, "2", "2")
         make_button_grid(get[1], "조회", 1, 5, "4", "2")
@@ -71,29 +105,28 @@ def main():
     def make_popup_path():
         global global_font
         get = make_popup("훈련소 가는 길")
-        # get[0].xml_connect()
+        listbox = make_listbox(get[1], 0, 5)
+        for i, spot in enumerate(training_spot):
+            listbox.insert(i, spot)
 
-        InputLabel = Entry(get[1], font = global_font, width = 25, borderwidth = 12, relief = 'flat')
-        InputLabel.grid(row = 0, column = 5)
+        def view():
+            choose = listbox.curselection()[0] + 47
+            webbrowser.open("https://www.mma.go.kr/contents.do?mc=mma00020" + "{0}".format(choose))
 
-        make_button_grid(get[1], "검색", 1, 4, "4", "2")
-        make_button_grid(get[1], "조회", 1, 5, "4", "2")
-        make_button_grid(get[1], "청소", 1, 6, "4", "2")
+        make_button_grid(get[1], "바로가기", 1, 5, "8", "2", view)
         return get[1]
 
     def make_popup_pubinfo():
         get = make_popup("사회 복무 정보")
-        get[0].xml_connect("http://apis.data.go.kr/1300000/bmggJeongBo/list", "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D", urllib.parse.quote("경남"))
+        get[0].xml_connect("http://apis.data.go.kr/1300000/bmggJeongBo/list",
+                           "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D",
+                           urllib.parse.quote("경남"))
 
-        check = make_popup("공석 조회")
-        check[0].xml_connect("http://apis.data.go.kr/1300000/bistGongseok/list/bistGongseok/list",
-                             "4954u%2BzYV4y%2F5BRah3wXrxdhkbCaLFoKjzT7dLDNPzn44g%2BUeL30JEGzj2MitqPY9PMyqdb8yW4%2F8eo4xB1xYw%3D%3D", urllib.parse.quote("경남"))
+        listbox = make_listbox(get[1], 0, 5)
+        InputLabel = Entry(get[1], font=global_font, width=25, borderwidth=12, relief='flat')
+        InputLabel.grid(row=1, column=5)
 
-        InputLabel = Entry(get[1], font = global_font, width = 25, borderwidth = 12, relief = 'flat')
-        InputLabel.grid(row = 0, column = 5)
-
-        datalist = dict(bjdsgg = [], bokmuGgm = [], dpBokmuGgm = [], jeonhwaNo = [], sbjhjilbyeong = [], gtcdNm = [])
-        checklist = dict(bjdsggjusoNm = [], ghjbcNm = [], bmgigwanNm = [], shbmsojipDt = [])
+        datalist = dict(bjdsgg=[], bokmuGgm=[], dpBokmuGgm=[], jeonhwaNo=[], sbjhjilbyeong=[], gtcdNm=[])
 
         # datalist_bjdsgg = [] # 지역
         # datalist_bokmuGgm = [] #  복무기관명
@@ -117,24 +150,24 @@ def main():
                 print("전화번호 : {0}".format(datalist["jeonhwaNo"][i]))
                 print("기피질병 : {0}".format(datalist["sbjhjilbyeong"][i]))
 
-        make_button_grid(get[1], "검색", 1, 4, "4", "2")
-        make_button_grid(get[1], "조회", 1, 5, "4", "2")
-        make_button_grid(get[1], "청소", 1, 6, "4", "2")
+        make_button_grid(get[1], "검색", 2, 4, "4", "2")
+        make_button_grid(get[1], "조회", 2, 5, "4", "2")
+        make_button_grid(get[1], "청소", 2, 6, "4", "2")
         return get[1]
 
     def make_popup_calculator():
         get = make_popup("근무 일자 계산")
-        InputLabel = Entry(get[1], font = global_font, width = 25, borderwidth = 12, relief = 'flat')
+        InputLabel = Entry(get[1], font=global_font, width=25, borderwidth=12, relief='flat')
         InputLabel.pack()
-        InputLabel.place(x = 14, y = 96)
+        InputLabel.place(x=14, y=96)
 
         return get[1]
 
-    Caption = Label(window, font = global_font, text = "작업", background = '#ffffff')
-    Caption.place(x = 10, y = 20)
+    Caption = Label(window, font=global_font, text="작업", background='#ffffff')
+    Caption.place(x=10, y=20)
 
     buttons["military"] = make_button(window, "현역 정보", 280, 80, "18", "7", make_popup_military)
-    buttons["milinfo"] = make_button(window, "현역 정보", 280, 300, "18", "7", make_popup_milinfo)
+    buttons["milinfo"] = make_button(window, "현역 구비서류", 280, 300, "18", "7", make_popup_milinfo)
     buttons["path"] = make_button(window, "훈련소 가는 길", 488, 80, "18", "7", make_popup_path)
     buttons["pubinfo"] = make_button(window, "사회 복무 정보", 488, 300, "18", "7", make_popup_pubinfo)
     buttons["calculator"] = make_button(window, "근무 일자\n계산", 8, 442, "10", "3", make_popup_calculator)
